9529c6e508dc66af347bb32668e4b5cc37916a65
Cedar Daniel <cedardaniel1998@gmail.com>
2021-03-29 14:11:20 +0100

    implemented cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const bookRouter = require('./routes/bookRoutes');
 
 app.use(express.json());
 app.use(cors());
-
+app.options('*', cors());
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
